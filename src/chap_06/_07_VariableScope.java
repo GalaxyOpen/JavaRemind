35ec7e2748038e601355cf9d512e6f792a938dc8
Administrator <keehong11@naver.com>
2024-03-19 18:25:23 +0900

    자바 복귀 8일째

---
+++ src/chap_06/_07_VariableScope.java
@@ -0,0 +1,27 @@
+package chap_06;
+
+public class _07_VariableScope {
+//    public static void methodA(){
+//        System.out.println(number);
+//    }
+
+//    public static void methodA(){
+//        int result = 1; // 지역 변수
+//    }
+
+    public static void main(String[] args) {
+
+        // 변수의 범위
+        int number = 3;
+//        System.out.println(result);
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+//        System.out.println(i);
+        {
+            int j = 0;
+            System.out.println(j);
+        }
+//        System.out.println(j);
+    }
+}
